480e36ac4d8af0806822551fad6f6c23bf25ce19
ittow <phuocsangphang@gmail.com>
2025-11-19 18:04:44 +0700

    Hôm nay đã học thêm được một ít
    Chúng ta có thêm một số hàm thử nghiệm logic

--- aws/src/main.rs
+++ aws/src/main.rs
@@ -1,11 +1,11 @@
+use std::io;
 mod support;
 
-fn main() {
+fn d001() {
     let mut s: String = String::from("Hello, world");
     println!("{}", s);
-    
-    s.push_str("!");
-    
+
+    s.push_str("!");    
     println!("String {}", s);
     println!("Size: {}", s.len());
     println!("Length: {}", s.chars().count());
@@ -15,4 +15,21 @@ fn main() {
 
     let c: i64 = support::add(a, b);
     println!("Total: {}", c);
+
+    let mut i: String = String::new();
+    io::stdin().read_line(&mut i).expect("Hãy nhập gì đó");
+    if i == "" { print!("i = {i}"); }
+
+    let t: &str = "100.";
+    let b1: bool = support::isdigi(t);
+    let b2: bool = support::isdigf(t);
+    if b1 == true { println!("t là số nguyên"); }
+    if b2 == true { println!("t là số thực"); }
+
+    let x: f64 = support::sqal(2 as f64, 5);
+    println!("x = {x}");
+}
+
+fn main() {
+    d001();
 }
--- aws/src/support.rs
+++ aws/src/support.rs
@@ -1,3 +1,26 @@
 pub fn add(a: i64, b: i64) -> i64 {
     return a + b;
 }
+
+pub fn sqal(n: f64, b: i8) -> f64 {
+    let mut t = n;
+    for _ in 1..b {
+        t *= n;
+    }
+    return t;
+}
+
+pub fn isdigi(s: &str) -> bool {
+    for v in s.chars() {
+        if !('0' <= v && v <= '9') {return false;}
+    }
+    return true;
+}
+
+pub fn isdigf(s: &str) -> bool {
+    for (i, v) in s.chars().enumerate() {
+        if i == 0 && v == '.' || i == s.chars().count() - 1 && v == '.' {continue}
+        if !('0' <= v && v <= '9') {return false;}
+    }
+    return true;
+}
