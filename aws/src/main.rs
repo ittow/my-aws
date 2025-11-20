7fcd2cf52d7b720c7a3aa03cf92c0268367f99b7
ittow <phuocsangphang@gmail.com>
2025-11-20 16:31:09 +0700

    Mỗi ngày, hãy chậm rãi
    Hôm nay viết được một thuật toán nhỏ và công việc mở file bằng code

--- aws/src/main.rs
+++ aws/src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
+
+use crate::support::_filesystem;
 mod support;
 
-fn d001() {
+fn _d001() {
     let mut s: String = String::from("Hello, world");
     println!("{}", s);
 
@@ -13,7 +15,7 @@ fn d001() {
     let a: i64 = 10;
     let b: i64 = 20;
 
-    let c: i64 = support::add(a, b);
+    let c: i64 = support::_add(a, b);
     println!("Total: {}", c);
 
     let mut i: String = String::new();
@@ -21,15 +23,28 @@ fn d001() {
     if i == "" { print!("i = {i}"); }
 
     let t: &str = "100.";
-    let b1: bool = support::isdigi(t);
-    let b2: bool = support::isdigf(t);
+    let b1: bool = support::_isdigi(t);
+    let b2: bool = support::_isdigf(t);
     if b1 == true { println!("t là số nguyên"); }
     if b2 == true { println!("t là số thực"); }
 
-    let x: f64 = support::sqal(2 as f64, 5);
+    let x: f64 = support::_sqal(2 as f64, 5);
     println!("x = {x}");
 }
 
+fn _d002() {
+    let mut array: [i64; 10] = [3, 7, 4, 9, 2, 8, 5, 6, 0, 1];
+    support::_lrsorted(&mut array);
+    for a in array { print!("{a} ") }
+    println!();
+    
+    support::_rlsorted(&mut array);
+    for a in array { print!("{a} ") }
+    println!();
+
+    _filesystem();
+}
+
 fn main() {
-    d001();
+    _d002();
 }
--- aws/src/support.rs
+++ aws/src/support.rs
@@ -1,8 +1,10 @@
-pub fn add(a: i64, b: i64) -> i64 {
+use std::fs;
+
+pub fn _add(a: i64, b: i64) -> i64 {
     return a + b;
 }
 
-pub fn sqal(n: f64, b: i8) -> f64 {
+pub fn _sqal(n: f64, b: i8) -> f64 {
     let mut t = n;
     for _ in 1..b {
         t *= n;
@@ -10,17 +12,55 @@ pub fn sqal(n: f64, b: i8) -> f64 {
     return t;
 }
 
-pub fn isdigi(s: &str) -> bool {
+pub fn _isdigi(s: &str) -> bool {
     for v in s.chars() {
         if !('0' <= v && v <= '9') {return false;}
     }
     return true;
 }
 
-pub fn isdigf(s: &str) -> bool {
+pub fn _isdigf(s: &str) -> bool {
     for (i, v) in s.chars().enumerate() {
         if i == 0 && v == '.' || i == s.chars().count() - 1 && v == '.' {continue}
         if !('0' <= v && v <= '9') {return false;}
     }
     return true;
+}
+
+pub fn _lrsorted(array: &mut [i64]) {
+    array::sorted(array, true);
+}
+
+pub fn _rlsorted(array: &mut [i64]) {
+    array::sorted(array, false);
+}
+
+mod array {
+    pub fn sorted(array: &mut [i64], lr: bool) {
+        let n: usize = array.len();
+        for i in 0..n {
+            for j in 0..n - i - 1 {
+                let temp: i64 = array[j];
+                if lr {
+                    if array[j] > array[j+1] {
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                } else {
+                    if array[j] < array[j+1] {
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn _filesystem() {
+    const PATH: &str = "text.txt";
+    let content: String = fs::read_to_string(PATH)
+        .expect("Có lỗi xảy ra: có thể file không tồn tại, hoặc đường dẫn không đúng.");
+
+    println!("{content}");
 }
